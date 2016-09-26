40d42ddad679d04488868169764f0e21b24091c8
TK <Kirk.shilling@gmail.com>
2016-09-25 22:35:30 -0400

    added board class. Obvious initialization complete.

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ CONFIG_NAME = 'default_config.cfg'
 
 
 class Domino(object):
-    def __init__(self, side_1, side_2):
-
+    def __init__(self, side_1, side_2, type="normal"):
+        self.type = type
         self.side_1 = side_1
         self.side_2 = side_2
 
@@ -36,14 +36,28 @@ class Domino(object):
         return self.side_1 + self.side_2
 
 
+class Board(object):
+    # deque?
+
+    def __init__(self):
+        self.end1 = None
+        self.end2 = None
+
+    def is_empty(self):
+        if not self.end1:
+            return True
+        else:
+            return False
+
+
 class Player(object):
-    def __init__(self, order):
+    def __init__(self, order, game):
+        self.board = game.board
         self.order = order
-        self.dominoes = []
-        self.exposed = (None, None)
+        self.dominoes = set()
 
     def add_domino(self, domino):
-        self.dominoes.append(domino)
+        self.dominoes.add(domino)
 
     def check_for_completion(self):
         if self.dominoes:
@@ -54,30 +68,56 @@ class Player(object):
     def add_remaining_dominoes(self):
         return sum((domino.size for domino in self.dominoes))
 
+    def make_move(self):
+        if self.board.is_empty():
+            d = self.dominoes.pop()
+            self.board.end1 = d.side1
+            self.board.end2 = d.side2
+
 
 class Game(object):
-    def __init__(self, size):
-        dominoes = self._create_dominoes(size)
 
-    def _create_dominoes(self, size):
+    def __init__(self, size):
+        self.board = Board()
+        self.player_set = [Player(order, self) for order in range(4)]
+
+        dominoes = self._create_domino_set(size)
+
+        for player in self.player_set:
+            for count in range(7):
+                player.add_domino(dominoes.pop())
+
+    @staticmethod
+    def _create_domino_set(size):
         dominoes = set()
+        doubles = set()
         for i in range(size + 1):
             for j in range(size + 1):
+                size = i + j
+                if (i == j) and (not size in doubles):
+                    dominoes.add(Domino(i, j, "double"))
+                    doubles.add(size)
                 dominoes.add(Domino(i, j))
         return dominoes
 
-    def _create_players(self):
-        number = 1
+    # @staticmethod
+    # def _create_players():
+    #     player_set = set()
+    #     for order in range(1, 5):
+    #         p = Player(order)
+    #         player_set.add(p)
+    #     return player_set
 
-    def start(self):
-        pass
+    def run(self):
+        while True:
+            for player in self.player_set:
+                player.play_card()
 
 
-def run():
-
+def main():
     max_size = 6  # config['DEFAULT_SIZE']
     game = Game(max_size)
-    game.start()
+    game.run()
 
 
 def make_config_file(config, config_name):
@@ -95,7 +135,7 @@ if __name__ == "__main__":
         pass
 
     try:
-        attempt = run()
+        attempt = main()
     except KeyboardInterrupt:
         sys.exit(-1)
     except SystemError:
